a952a00498415a8462457737706e131cd7a62feb
SClasen95 <simonclasen1@outlook.com>
2020-11-23 15:43:43 -0300

    Initial commit

---
+++ keyGen/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keyGen {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            int i = (int)this.numericUpDown1.Value;
+            var queryBuilder = DBConnection.getInstance().getQueryBuilder();
+            
+           while (i>0) {
+                queryBuilder.setQuery("INSERT INTO claves (clave,estado) VALUES (@clave,@estado)");
+                queryBuilder.addParam("@clave", KeyGen.generarKey());
+                queryBuilder.addParam("@estado", "Disponible");
+                DBConnection.getInstance().abm(queryBuilder);
+                i--;
+            }
+
+        }
+    }
+}
---
+++ keyGen/KeyGen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyGen {
+    public static class KeyGen {
+
+
+        public const String values = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static Random r = new Random();
+
+        public static String generarKey() {
+            String ret = "";
+            int i = 16;
+            while (i > 0) {
+                if (i != 16 && i % 4 == 0) {
+                    ret += "-";
+                }
+                int index = r.Next(0, values.Length);
+                ret += values[index];
+                i--;
+            }
+            return ret;
+        }
+        public static List<String> generarKey(int i) {
+            List<String> ret = new List<string>();
+            int j = 16;
+            String key = "";
+            while (i > 0) {
+                while (j > 0) {
+                    if (j != 16 && j % 4 == 0) {
+                        key += "-";
+                    }
+                    int index = r.Next(0, values.Length);
+                    key += values[index];
+                    j--;
+                }
+                ret.Add(key);
+                i--;
+            }
+            return ret;
+        }
+    }
+}
